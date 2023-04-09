9068544ec7433a66a07bce6daf83e0c0f3efb5c8
Zaur <zaur.cherchesov@mail.ru>
2023-04-09 17:49:11 +0300

    finished Task 13

--- HomeWork02 C#/Task02/Program.cs
+++ HomeWork02 C#/Task02/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число для начала интервала");
+int numberA = int.Parse (Console.ReadLine());
+Console.WriteLine("Введите число для конца интервала");
+int numberB = int.Parse (Console.ReadLine());
+int Rand = new Random().Next(numberA, numberB);
+Console.WriteLine(Rand);
+
+if (Rand > 100 && Rand < 1000)
+{
+    Rand %= 10;
+    Console.WriteLine(Rand);
+}
+else if (Rand > 1000)
+{
+    string str = Rand.ToString();
+    Console.WriteLine(str[2]);
+}
+else
+{
+    Console.WriteLine("Третей цифры нет");
+}
