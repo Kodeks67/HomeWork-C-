2fadad9c3093fe43fc6a24794d5153f436f80f54
Zaur <zaur.cherchesov@mail.ru>
2023-04-09 16:42:52 +0300

    finished Task 15

--- HomeWork02 C#/Task03/Program.cs
+++ HomeWork02 C#/Task03/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = int.Parse (Console.ReadLine());
+
+if (numberA <= 5 || numberA >= 8)
+{
+    Console.WriteLine("No");
+}
+else
+{
+    Console.WriteLine("Yes");
+}
