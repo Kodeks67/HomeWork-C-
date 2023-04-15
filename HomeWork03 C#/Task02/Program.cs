294be94cabec7f94af0d8d77d43cb9b24eb5eea7
Zaur <zaur.cherchesov@mail.ru>
2023-04-15 17:44:03 +0300

    solved task 21

--- HomeWork03 C#/Task02/Program.cs
+++ HomeWork03 C#/Task02/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double Dist()
+{  
+Console.WriteLine("Введите А(x, y, z)");
+double numA_x = double.Parse (Console.ReadLine());
+double numA_y = double.Parse (Console.ReadLine());
+double numA_z = double.Parse (Console.ReadLine());
+Console.WriteLine("Введите Б(x, y, z)");
+double numB_x = double.Parse (Console.ReadLine());
+double numB_y = double.Parse (Console.ReadLine());
+double numB_z = double.Parse (Console.ReadLine());
+double result = Math.Sqrt(Math.Pow(numA_x - numB_x, 2) + Math.Pow(numA_y - numB_y, 2) + Math.Pow(numA_z - numB_z, 2));
+return result;
+}
+Console.WriteLine(Dist());
