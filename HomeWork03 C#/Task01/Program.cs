66c615059dfc23636d4635c5cecc7164945c6251
Zaur <zaur.cherchesov@mail.ru>
2023-04-15 18:10:21 +0300

    solved task 19

--- HomeWork03 C#/Task01/Program.cs
+++ HomeWork03 C#/Task01/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿bool palindrome(int num)
+{
+    if (num / 10000 == num % 10 && (num / 1000) % 10 == (num / 10) % 10)
+    {
+        return true;
+    }
+    return false;
+
+        
+}
+Console.WriteLine(palindrome(int.Parse(Console.ReadLine())));
