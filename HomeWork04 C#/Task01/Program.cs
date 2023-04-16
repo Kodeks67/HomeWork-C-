d5c51d36b2ac725a39113ed2a202627093e92a44
Zaur <zaur.cherchesov@mail.ru>
2023-04-16 21:30:03 +0300

    added HomeWork04 and added folders with tasks to HomeWork04 and solved task25

---
+++ HomeWork04 C#/Task01/Program.cs
@@ -0,0 +1,10 @@
+﻿int NaturalDegree(int first_number, int second_number)
+{
+    int result = 1;
+    for (int i = 1; i <= second_number; i++)
+    {
+        result = result * first_number;
+    }
+    return result;
+}
+Console.WriteLine(NaturalDegree(5, 4));
