8d452e4a05122e4a850729732cdeac547fb4a541
Zaur <zaur.cherchesov@mail.ru>
2023-04-16 22:20:12 +0300

    finished task29

--- HomeWork04 C#/Task03/Program.cs
+++ HomeWork04 C#/Task03/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] CreateRndArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+        // array[i] = int.Parse(Console.ReadLine()); -- записывает в столбик числа массива 
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine($"[{String.Join(", ", array)}]");
+}
+
+int leg = int.Parse(Console.ReadLine());
+int minValue = int.Parse(Console.ReadLine());
+int maxValue = int.Parse(Console.ReadLine());
+int[] arr = CreateRndArray(leg, minValue, maxValue);
+PrintArray(arr);
