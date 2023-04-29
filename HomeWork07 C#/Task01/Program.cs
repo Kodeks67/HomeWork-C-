5695e081c48fc589f3fe607069b99fb674280a7c
zaur <zaur.cherchesov@mail.ru>
2023-04-29 19:42:34 +0300

    Modification Task 01 line 12 created random double

--- HomeWork07 C#/Task01/Program.cs
+++ HomeWork07 C#/Task01/Program.cs
@@ -1,27 +1,37 @@
-﻿double[,] CreateTwoDimensionArray(int firstLength, int secondLength)
+﻿void RandomNumbers(int firstnumber, int secondnumber)
 {
-	double[,] array = new double[firstLength, secondLength];
+    int rnd = new Random().Next(firstnumber, secondnumber);
+}
 
-	for (int i = 0; i < array.GetLength(0); i++)
-		for (int j = 0; j < array.GetLength(1); j++)
-			array[i, j] = new Random().NextDouble();
+double[,] CreateTwoDimensionArray(int firstLength, int secondLength, int firstnumber, int secondnumber)
+{
+    double[,] array = new double[firstLength, secondLength];
 
-	return array;
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().NextDouble() * new Random().Next(firstnumber, secondnumber);
+
+    return array;
 }
 
 
 void PrintArray(double[,] array)
 {
-	for (int i = 0; i < array.GetLength(0); i++)
-	{
-		for (int j = 0; j < array.GetLength(1); j++)
-			Console.Write($"{array[i, j]} ");
-		Console.WriteLine();
-	}
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
 }
 
-Console.WriteLine("Введите количество строк");
+Console.Write("первое число для диапазона: ");
+int firstnumber1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число для диапазона: ");
+int secondnumber2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество строк: ");
 int line = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов");
+Console.Write("Введите количество столбцов: ");
 int column = Convert.ToInt32(Console.ReadLine());
-PrintArray(CreateTwoDimensionArray(line, column ));
+PrintArray(CreateTwoDimensionArray(line, column, firstnumber1, secondnumber2));
