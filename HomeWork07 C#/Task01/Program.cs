80391d83149d2b151abbc4bed595f4aa3b806aed
Zaur <zaur.cherchesov@mail.ru>
2023-04-29 15:57:06 +0300

    Finished Task 1

--- HomeWork07 C#/Task01/Program.cs
+++ HomeWork07 C#/Task01/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[,] CreateTwoDimensionArray(int firstLength, int secondLength)
+{
+	double[,] array = new double[firstLength, secondLength];
+
+	for (int i = 0; i < array.GetLength(0); i++)
+		for (int j = 0; j < array.GetLength(1); j++)
+			array[i, j] = new Random().NextDouble();
+
+	return array;
+}
+
+
+void PrintArray(double[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+			Console.Write($"{array[i, j]} ");
+		Console.WriteLine();
+	}
+}
+
+Console.WriteLine("Введите количество строк");
+int line = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int column = Convert.ToInt32(Console.ReadLine());
+PrintArray(CreateTwoDimensionArray(line, column ));
