a28458b63ff66ff8c2d73872b3fd142e7b97b1e8
Zaur <zaur.cherchesov@mail.ru>
2023-04-30 14:00:34 +0300

    finished Task 02 and 03

--- HomeWork07 C#/Task02/Program.cs
+++ HomeWork07 C#/Task02/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] CreateTwoDimensionArray(int firstLength, int secondLength)
+{
+    int[,] array = new int[firstLength, secondLength];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+
+    return array;
+}
+
+bool FillArray(int[,] array, int checkNumber)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            if (array[i, j] == checkNumber)
+            {
+                Console.WriteLine($"{checkNumber} - Ура в массиве такое число есть");
+                return true;
+            }
+
+    Console.WriteLine($"{checkNumber} - такого числа в массиве нет!");
+    return false;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+int[,] array = CreateTwoDimensionArray(4, 4);
+PrintArray(array);
+Console.Write("Введите проверяемое число: ");
+int checkNumber = Convert.ToInt32(Console.ReadLine());
+FillArray(array, checkNumber);
--- HomeWork07 C#/Task03/Program.cs
+++ HomeWork07 C#/Task03/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] CreateTwoDimensionArray(int firstLength, int secondLength)
+{
+    int[,] array = new int[firstLength, secondLength];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+
+    return array;
+}
+
+void arithmeticMean(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double sumColumn = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
+            {
+            // sumColumn = sumColumn + index(array[0, 1])
+            sumColumn += array[j, i];
+            }
+        Console.WriteLine($"столбец {i} - {sumColumn / array.GetLength(0)} ");
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите количество строк: ");
+int line = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] array = CreateTwoDimensionArray(line, column);
+PrintArray(array);
+Console.WriteLine("");
+arithmeticMean(array);
